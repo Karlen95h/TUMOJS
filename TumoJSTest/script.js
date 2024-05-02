fdae11143b9d54d38b6d537fc5dc8fad86d26fda
Karlen <85682913+Karlen95h@users.noreply.github.com>
2024-05-02 14:25:56 +0400

    chacges

---
+++ TumoJSTest/script.js
@@ -0,0 +1,22 @@
+//Պատրաստ ֆունկցիաներ
+//left()
+//color()
+//width()
+//forward()
+
+
+// color ("aqua")
+// width (10)
+// for(let i = 0 ; i < 6; i++){
+//         forward (150)
+//         left (60)
+// }
+
+///
+
+// for(let i = 0 ; i < 360; i++){
+//     color ("red")
+//    forward(1)
+//    left (1)
+// }
+
